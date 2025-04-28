ccc40729d236ceda8ac2d58939a4a5b24762e9f6
chvj7567 <chvj7567@naver.com>
2025-04-28 23:38:08 +0900

    [System] 카드 시스템 수정

--- Assets/Scripts/System/CardSystem.cs
+++ Assets/Scripts/System/CardSystem.cs
@@ -12,16 +12,41 @@ namespace CHCard
 
     public class CardSystem
     {
-        Dictionary<int, CardSystem> _dicCard = null;
+        Dictionary<int, CardData> _dicCard = null;
 
         public CardSystem()
         {
-            _dicCard = new Dictionary<int, CardSystem>();
+            _dicCard = new Dictionary<int, CardData>();
         }
 
         public void Clear()
         {
             _dicCard.Clear();
+        }
+
+        public void AddCard(int number, int count = 1)
+        {
+            if (_dicCard.TryGetValue(number, out CardData cardData) == false)
+            {
+                cardData = new CardData();
+                cardData.number = number;
+                cardData.count = count;
+            }
+            else
+            {
+                cardData.count += count;
+            }
+        }
+
+        public void RemoveCard(int number, int count = 1)
+        {
+            if (_dicCard.TryGetValue(number, out CardData cardData) == false)
+                return;
+
+            if (cardData.count <= 0)
+                return;
+
+            cardData.count -= count;
         }
     }
 }
