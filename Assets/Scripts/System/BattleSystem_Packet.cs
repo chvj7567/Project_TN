6265dc996cab2e0028a012f31a92569971edd601
chvj7567 <chvj7567@naver.com>
2025-05-04 23:27:35 +0900

    [System] 배틀 네임스페이스 추가

--- Assets/Scripts/System/BattleSystem_Packet.cs
+++ Assets/Scripts/System/BattleSystem_Packet.cs
@@ -1,59 +1,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public partial class BattleSystem
+namespace CHBattle
 {
-    #region Request/Response
-    public void RequestMatchBattle()
+    public partial class BattleSystem
     {
-        //# 배틀 매칭 패킷
+        #region Request/Response
+        public void RequestMatchBattle()
+        {
+            //# 배틀 매칭 패킷
+        }
+
+        public void ResponseMatchBattle()
+        {
+
+        }
+
+        public void RequestSelectCardDeck(List<int> liCardDeck)
+        {
+            //# 덱 선택 패킷
+        }
+
+        public void ResponseSelectCardDeck()
+        {
+
+        }
+
+        public void RequestUseCard(int cardNumber)
+        {
+            //# 카드 사용 패킷
+        }
+
+        public void ResponseUseCard()
+        {
+
+        }
+        #endregion Request/Response
+
+        #region Notice
+        public void NoticeStartBattle()
+        {
+            //# 배틀 시작 패킷
+        }
+
+        public void NoticeBattleRoundResult()
+        {
+            //# 배틀 라운드 결과 패킷
+        }
+
+        public void NoticeBattleFinalResult()
+        {
+            //# 배틀 최종 결과 패킷
+        }
+
+        public void NoticeBattleMission()
+        {
+            //# 배틀 미션 패킷
+        }
+        #endregion
     }
-
-    public void ResponseMatchBattle()
-    {
-
-    }
-
-    public void RequestSelectCardDeck(List<int> liCardDeck)
-    {
-        //# 덱 선택 패킷
-    }
-
-    public void ResponseSelectCardDeck()
-    {
-
-    }
-
-    public void RequestUseCard(int cardNumber)
-    {
-        //# 카드 사용 패킷
-    }
-
-    public void ResponseUseCard()
-    {
-
-    }
-    #endregion Request/Response
-
-    #region Notice
-    public void NoticeStartBattle()
-    {
-        //# 배틀 시작 패킷
-    }
-
-    public void NoticeBattleRoundResult()
-    {
-        //# 배틀 라운드 결과 패킷
-    }
-
-    public void NoticeBattleFinalResult()
-    {
-        //# 배틀 최종 결과 패킷
-    }
-
-    public void NoticeBattleMission()
-    {
-        //# 배틀 미션 패킷
-    }
-    #endregion
 }
